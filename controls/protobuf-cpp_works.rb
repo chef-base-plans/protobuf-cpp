a35218b7f3f0b258dced7f69ffdd3d0f13208568
Steven Marshall <SMarshall@chef.io>
2020-06-23 13:29:23 +0100

    [protobuf-cpp] Various refactors of existing code
    Added inspec.lock to .gitignore
    Updated README.md to latest binary format
    Added plan_name to attributes.yml
    Updates controls to add more prescriptive version checking.
    Updated inspec.yml to latest version
    
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/protobuf-cpp_works.rb
+++ controls/protobuf-cpp_works.rb
@@ -9,28 +9,34 @@ control 'core-plans-protobuf-cpp' do
   impact 1.0
   title 'Ensure protobuf-cpp binary is working as expected'
   desc '
-  We first check that the protobuf-cpp binary we expect is present and then run version checks on both to verify that it is excecutable.
+  To test the protoc binary that protobuf-cpp provides we first check for the installation directory.
+  Using this directory we then run checks to ensure the binary exists.
+  Then we test that the version of the binary we expect to be installed exists.
+    $ $PKG_PATH/bin/protoc --version
+      libprotoc 3.9.2
   '
 
   hab_pkg_path = command("hab pkg path #{plan_ident}")
   describe hab_pkg_path do
-    its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
   end
 
   target_dir = File.join(hab_pkg_path.stdout.strip, base_dir)
 
-  protoc_exists = command("ls #{File.join(target_dir, "protoc")}")
+  protoc_exists = command("ls -al #{File.join(target_dir, "protoc")}")
   describe protoc_exists do
     its('stdout') { should match /protoc/ }
     its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
 
-  protoc_works = command("/bin/protoc --version")
+  protoc_works = command("#{File.join(target_dir, "protoc")} --version")
   describe protoc_works do
-    its('stdout') { should match /libprotoc [0-9]+.[0-9]+.[0-9]+/ }
+    its('stdout') { should match /libprotoc #{hab_pkg_path.stdout.strip.split('/')[5]}/ }
     its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
+
 end
